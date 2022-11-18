bb16e2bb3ffa9546ad0da06b70e1630e5f49f4f9
gusongen <1572055146@qq.com>
2022-11-18 20:28:45 +0800

    baseline v0.1

--- src/utils.py
+++ src/utils.py
@@ -25,12 +25,12 @@ def generate_neg_sample(train_data, neg_samples_size):
 
 def load_data(graph1, graph2, anoise):
     # load graph adjacent matrix by networkx
-    A1 = nx.read_edgelist(f"{graph1}.txt", nodetype = int, comments="%")
-    adj1 = nx.adjacency_matrix(A1, nodelist = range(A1.number_of_nodes()))
-    A1=np.array(nx.adjacency_matrix(A1).todense())
-    A2 = nx.read_edgelist(f"{graph2}.txt", nodetype = int, comments="%")
-    adj2 = nx.adjacency_matrix(A2, nodelist = range(A2.number_of_nodes()))
-    A2=np.array(nx.adjacency_matrix(A2).todense())
+    # A1 = nx.read_edgelist(f"{graph1}.txt", nodetype = int, comments="%")
+    # adj1 = nx.adjacency_matrix(A1, nodelist = range(A1.number_of_nodes()))
+    # A1=np.array(nx.adjacency_matrix(A1).todense())
+    # A2 = nx.read_edgelist(f"{graph2}.txt", nodetype = int, comments="%")
+    # adj2 = nx.adjacency_matrix(A2, nodelist = range(A2.number_of_nodes()))
+    # A2=np.array(nx.adjacency_matrix(A2).todense())
     # load graph adjacent matrix by numpy.npy
     A1 = np.load(f"data/graph/{graph1}.npy")
     A2 = np.load(f"data/graph/{graph2}.npy")
--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-from multiprocessing import reduction
 import os
 import math
 import torch
@@ -15,6 +14,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import euclidean_distances
 import argparse
 np.set_printoptions(suppress=True)
+# tensorboard
+from tensorboardX import SummaryWriter
 
 from src.loss import customized_loss, margin_ranking_loss
 from src.dataset import Dataset
@@ -26,8 +27,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"current device is {device}")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epoch", type=int, default=100, help="epoch to run")
-parser.add_argument("--seed", type=int, default=3, help="training set ratio")
+parser.add_argument("--epoch", type=int, default=500, help="epoch to run")
+parser.add_argument("--seed", type=int, default=8, help="training set ratio")
 parser.add_argument('--hidden', type=int, default=128, help="hidden dimension of entity embeddings")
 parser.add_argument('--lr', type=float, default=0.01, help="learning rate")
 parser.add_argument('--k', type=float, default=10, help="hit@k")
@@ -37,6 +38,7 @@ parser.add_argument('--weight_decay', type=float, default=1e-5, help="weight dec
 parser.add_argument('--graph_s', type=str, default="data_G1", help="source graph path")
 parser.add_argument('--graph_d', type=str, default="data_G2", help="destination graph path")
 parser.add_argument('--anoise', type=float, default=0.2, help="anchor noise")
+parser.add_argument('--board_path', type=str, default='board', help="tensorboard path")
 args = parser.parse_args()
 
 ############################
@@ -53,14 +55,15 @@ train_seeds_ratio = args.seed * 0.1
 k = args.k
 anoise = args.anoise
 ############################
+tb_logger = SummaryWriter(args.board_path)
 
 ############################
 # preprocess
 graph1 = graph_path_s
 graph2 = graph_path_d
 A1, A2, anchor = load_data(graph1=graph1, graph2=graph2, anoise=anoise)
-train_size = int(train_seeds_ratio * len(anchor[:,0]))
-test_size = len(anchor[:,0]) - train_size
+train_size = int(train_seeds_ratio * len(anchor[:, 0]))
+test_size = len(anchor[:, 0]) - train_size
 train_set, test_set = torch.utils.data.random_split(anchor, lengths=[train_size, test_size])
 train_set = np.array(list(train_set))
 test_set = np.array(list(test_set))
@@ -76,6 +79,35 @@ pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_g
 print(f'#total params: {pytorch_total_params}')
 print(f"training samples: {train_size}, test samples: {test_size}")
 print(f"model architecture:\n {model}")
+
+
+def predict(output_file, sim_measure="cosine",):
+    """
+    将预测写入文件
+    """
+    model.eval()
+    Embedding1, Embedding2 = model()
+    Embedding1 = Embedding1.detach()
+    Embedding2 = Embedding2.detach()
+
+    # step 1: generate sim mat
+    if sim_measure == "cosine":
+        # similarity_matrix = cosine_similarity(Embedding1, Embedding2)
+        similarity_matrix = torch.mm(Embedding1, Embedding2.t()).cpu().numpy()
+    else:
+        Embedding1 = Embedding1.numpy()
+        Embedding2 = Embedding2.numpy()
+        similarity_matrix = euclidean_distances(Embedding1, Embedding2)
+        similarity_matrix = np.exp(-similarity_matrix)
+    # step 2: information statistics
+    # alignment_hit1 = list()
+    file = open(output_file, 'w')
+    for idx0, line in enumerate(similarity_matrix):
+        idx = np.argmax(line)
+        idx = int(idx)
+        # alignment_hit1.append(idx)
+        file.write(f'{idx0} {idx}\n')
+
 
 def evaluate(data, k, sim_measure="cosine", phase="test"):
     model.eval()
@@ -113,6 +145,7 @@ def evaluate(data, k, sim_measure="cosine", phase="test"):
             hit_k_score += 1
     return similarity_matrix, alignment_hit1, alignment_hitk, hit_1_score, hit_k_score
 
+
 # begin training
 best_E1 = None
 best_E2 = None
@@ -128,16 +161,24 @@ for e in range(epoch):
         optimizer.zero_grad()
         loss = customized_loss(E1, E2, a1_align, a2_align, neg1_left, neg1_right, neg2_left, neg2_right, neg_samples_size=neg_samples_size, neg_param=0.3)
         # loss = margin_ranking_loss(criterion, E1, E2, a1_align, a2_align, neg1_left, neg1_right, neg2_left, neg2_right)
-        loss.backward() # print([x.grad for x in optimizer.param_groups[0]['params']])
+        loss.backward()  # print([x.grad for x in optimizer.param_groups[0]['params']])
         optimizer.step()
         sim_mat, alignment_hit1, alignment_hitk, hit_1_score, hit_k_score = evaluate(data=test_set, k=k)
+
         if hit_1_score > best_hit_1_score:
             best_hit_1_score = hit_1_score
+            # todo save model
             print(f"current best Hits@1 count at the {e+1}th epoch: {best_hit_1_score}")
+
+    tb_logger.add_scalar('loss_train', loss.item(), epoch)
     print(f"epoch: {e+1}, loss: {round(loss.item(), 3)}\n")
 
 # final evaluation and test
-ground_truth = np.loadtxt('ground_truth.txt', delimiter=' ')
-similarity_matrix, alignment_hit1, alignment_hitk, hit_1_score, hit_k_score = evaluate(data=ground_truth, k=k, phase="over")
-print(similarity_matrix)
-print(f"final score: hit@1: total {hit_1_score} and ratio {round(hit_1_score/len(ground_truth), 2)}, hit@{k}: total {hit_k_score} and ratio {round(hit_k_score/len(ground_truth), 2)}")
+# ground_truth = np.loadtxt('ground_truth.txt', delimiter=' ')
+# ground_truth = np.loadtxt('data/anchor/anchor_0.2_test.txt', delimiter=' ')
+# similarity_matrix, alignment_hit1, alignment_hitk, hit_1_score, hit_k_score = evaluate(data=ground_truth, k=k, phase="over")
+# print(similarity_matrix)
+# print(f"final score: hit@1: total {hit_1_score} and ratio {round(hit_1_score/len(ground_truth), 2)}, hit@{k}: total {hit_k_score} and ratio {round(hit_k_score/len(ground_truth), 2)}")
+
+# 写入文件
+predict(f'submit_tmp_{args.graph_s}_{args.graph_d}_{anoise}.txt',)
